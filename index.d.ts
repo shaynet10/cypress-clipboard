a843aa838cb5a592cf9dc7afa19ff9a060ef6995
Shay Gueta <shaynet10@gmail.com>
2023-06-18 20:15:03 +0300

    updating index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,10 +1,6 @@
 declare namespace Cypress {
     interface Chainable {
-        map(callbackFn): Chainable<any[]>;
-        reduce(callbackFn, initialValue?: any): Chainable<any>;
-        every(callbackFn): Chainable<boolean>;
-        join(delimiter?: string): Chainable<string>;
-        reverse(): Chainable<any>;
+        copyToClipboard(): Chainable;
+        copyFromClipboard(): Chainable;
     }
 }
-
--- test/index.d.ts
+++ test/index.d.ts
@@ -1,7 +1,5 @@
 declare namespace Cypress {
     interface Chainable {
-        copyToClipboard(): Chainable;
-        copyFromClipboard(): Chainable;
     }
 }
 
